6bb1cd0c05e1c18ea77219c5b4ad1989c4efb534
digitalamnesia <mike@digitalamnesia.33mail.com>
2021-02-09 13:57:08 -0500

    configured axios request.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect, useMemo } from 'react';
 
 function App() {
-  return (
-    <div>
-      <h1>Hello...This is a place holder for now</h1>
-    </div>
-  );
-}
+  //init the loading  state to true
+  const [loadingData, setLoadingData] = useState(true);
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    async function getData() {
+        await axios
+            .get('https://randomuser.me/api/?results=50')
+            .then((response) => {
+                setData(response.data);
+                setLoadingData(false);
+                console.log(response.data);
+            });
+    }
+    if (loadingData) {
+        getData();
+    }
+    //and...this time...only run it once or you get rate banned again...
+  }, []);
+
+
+  return (
+    <div className='table-container'>
+            {loadingData ? (
+                <div>Loading...</div>
+            ) : (
+                <div>This is a placeholder</div>
+            )}
+        </div>
+    )
+  }
 export default App;
+
